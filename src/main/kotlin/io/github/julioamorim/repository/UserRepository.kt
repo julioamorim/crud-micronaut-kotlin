b37a388cce90ece8c62926315789e534fccd9692
Júlio Amorim <julio.l.amorim00@gmail.com>
2021-08-31 16:44:37 -0300

    Adding bean annotation to repository

--- src/main/kotlin/io/github/julioamorim/repository/UserRepository.kt
+++ src/main/kotlin/io/github/julioamorim/repository/UserRepository.kt
@@ -1,8 +1,10 @@
 package io.github.julioamorim.repository
 
 import io.github.julioamorim.domain.User
+import io.micronaut.data.annotation.Repository
 import io.micronaut.data.repository.CrudRepository
 
+@Repository
 interface UserRepository : CrudRepository<User, Long> {
 
     fun findByEmail(email: String): User?
