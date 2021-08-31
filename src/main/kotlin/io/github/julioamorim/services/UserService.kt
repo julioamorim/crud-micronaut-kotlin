e97f62ad069571f18ed6ef5850dbf88131eb8969
Júlio Amorim <julio.l.amorim00@gmail.com>
2021-08-31 14:33:27 -0300

    crud methods interface added

--- src/main/kotlin/io/github/julioamorim/services/UserService.kt
+++ src/main/kotlin/io/github/julioamorim/services/UserService.kt
@@ -1,4 +1,15 @@
 package io.github.julioamorim.services
 
+import io.github.julioamorim.model.Response
+import io.github.julioamorim.model.UserDTO
+
 interface UserService {
+
+    fun saveUser(user: UserService, response: Response<UserDTO>)
+
+    fun delete(userId: Long, response: Response<UserDTO>)
+
+    fun findAll(response: Response<List<UserDTO>>)
+
+    fun findById(userId: Long, response: Response<UserDTO>)
 }
