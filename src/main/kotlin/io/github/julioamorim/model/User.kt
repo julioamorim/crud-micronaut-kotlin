3d41b97c4bdc9572545c7115cbc12c11880f34e3
julioamorim <julio.l.amorim00@gmail.com>
2021-08-31 02:09:40 -0300

    model package with User and UserDTO created

---
+++ src/main/kotlin/io/github/julioamorim/model/User.kt
@@ -0,0 +1,17 @@
+package io.github.julioamorim.model
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "users")
+data class User(
+    @Id
+    @GeneratedValue
+    val id: Long = 0,
+    @Column(name = "name")
+    val name: String = "",
+    @Column(name = "password")
+    val password: String = "",
+    @Column(name = "email")
+    val email: String = ""
+)
---
+++ src/main/kotlin/io/github/julioamorim/model/UserDTO.kt
@@ -0,0 +1,27 @@
+package io.github.julioamorim.model
+
+data class UserDTO(
+    var name: String? = null,
+    var email: String? = null,
+    var password: String? = null
+) {
+    constructor(user: User) : this(
+        name = user.name,
+        email = user.email
+    )
+
+    fun toUse(): User? {
+        return if (validade()) {
+            User(
+                name = name!!,
+                email = email!!,
+                password = password ?: ""
+            )
+        }else{
+            null
+        }
+    }
+
+    fun validade() = !name.isNullOrEmpty() || !email.isNullOrEmpty()
+
+}
